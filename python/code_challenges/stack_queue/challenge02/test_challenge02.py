fa859f40c2ccbaf3543b15fbedb17e49396c9be5
alaa-adoos <om0331549@gmail.com>
2022-11-01 00:49:47 +0300

    challenge02

--- python/code_challenges/stack_queue/challenge02/test_challenge02.py
+++ python/code_challenges/stack_queue/challenge02/test_challenge02.py
@@ -1,1 +1,47 @@
 # Write your test here
+import pytest
+from challenge02 import *
+
+
+def test_is_Valid_closed_by_same_type_1():
+    s = "()"
+    actual= is_Valid(s)
+    expected =True
+    assert actual == expected
+
+def test_is_Valid_closed_by_same_type_2():
+    s = "()[]{}"
+    actual= is_Valid(s)
+    expected =True
+    assert actual == expected
+
+def test_is_Valid_closed_by_same_type_3():
+    s = "[({}]"
+    actual= is_Valid(s)
+    expected =False
+    assert actual == expected
+    
+def test_is_Valid_with_another_char():
+    s = "[(hello)()]"
+    actual= is_Valid(s)
+    expected =True
+    assert actual == expected
+
+def test_is_Valid_with_another_char_2():
+    s = "[{((s))}]"
+    actual= is_Valid(s)
+    expected =True
+    assert actual == expected
+
+def test_is_Valid_in_different_order():
+    s = "[{(({)})}]"
+    actual= is_Valid(s)
+    expected =False
+    assert actual == expected
+
+def test_is_Valid_start_with_close_bracket():
+    s = "}{(({)})}]"
+    actual= is_Valid(s)
+    expected =False
+    assert actual == expected
+    
---
+++ python/code_challenges/stack_queue/challenge02/whiteboard.md
@@ -0,0 +1,1 @@
+![stack](./white_board.jpg)
